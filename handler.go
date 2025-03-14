dbc526b1d90badeecf28bedd9dbcfdc4f1e7761a
frankschweitzer <schwfj20@wfu.edu>
2025-03-14 17:28:43 -0400

    finished H set/get

--- handler.go
+++ handler.go
@@ -7,10 +7,15 @@ import (
 var MAP = map[string]string{}
 var MAPmu = sync.RWMutex{}
 
+var HMAP = map[string]map[string]string{}
+var HMAPmu = sync.RWMutex{}
+
 var Handlers = map[string]func([]Value) Value{
 	"PING" : ping,
 	"SET" : set,
 	"GET" : get,
+	"HSET" : hset,
+	"HGET" : hget,
 }
 
 func ping(args []Value) Value {
@@ -50,5 +55,42 @@ func get(args []Value) Value {
 		return Value{typ:"error", str:"ERROR: COULD NOT GET VALUE"}
 	}
 
-	return Value{typ:"string", str:value}
+	return Value{typ:"bulk", bulk:value}
+}
+
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ:"error", str:"ERROR: WRONG NUMBER OF ARGUMENTS"}
+	}
+
+	table := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+
+	HMAPmu.Lock()
+	if _, ok := HMAP[table]; !ok {
+		HMAP[table] = map[string]string{}
+	}
+	HMAP[table][key] = value
+	HMAPmu.Unlock()
+
+	return Value{typ:"string", str:"OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ:"error", str:"ERROR: WRONG NUMBER OF ARGUMENTS"}
+	}
+
+	table := args[0].bulk
+	key := args[1].bulk
+
+	HMAPmu.Lock()
+	value, ok := HMAP[table][key]
+	HMAPmu.Unlock()
+	if !ok {
+		return Value{typ:"error", str:"ERROR: COULD NOT GET VALUE"}
+	}
+
+	return Value{typ:"bulk", bulk:value}
 }
